336ea2dda8b68ad95de96e6ce1f28ec39808312f
guanhongl <guanh.sl23@gmail.com>
2023-08-09 19:06:13 -1000

    BookInstance detail page
    -date_of_birth_formatted, date_of_death_formatted virtual prop
    +lifespan virtual prop

--- models/author.js
+++ models/author.js
@@ -28,14 +28,20 @@ AuthorSchema.virtual("url").get(function () {
   return `/catalog/author/${this._id}`;
 });
 
-AuthorSchema.virtual("date_of_birth_formatted").get(function () {
-  return this.date_of_birth ?
-    DateTime.fromJSDate(this.date_of_birth).toLocaleString(DateTime.DATE_MED) : '';
-});
+AuthorSchema.virtual("lifespan").get(function () {
+  let lifetime_string = "";
 
-AuthorSchema.virtual("date_of_death_formatted").get(function () {
-  return this.date_of_death ?
-    DateTime.fromJSDate(this.date_of_death).toLocaleString(DateTime.DATE_MED) : '';
+  if (this.date_of_birth) {
+    lifetime_string = DateTime.fromJSDate(this.date_of_birth).toLocaleString(DateTime.DATE_MED);
+  }
+
+  lifetime_string += " - ";
+
+  if (this.date_of_death) {
+    lifetime_string += DateTime.fromJSDate(this.date_of_death).toLocaleString(DateTime.DATE_MED);
+  }
+
+  return lifetime_string;
 });
 
 // Export model
